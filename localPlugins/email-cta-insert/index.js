8bcc2e089db02d3687ef798fb67609d7b7984404
Kevin Griffin <kevin@kevgriffin.com>
2020-05-25 14:24:39 -0400

    Adding AZF to support recaptcha

--- localPlugins/email-cta-insert/index.js
+++ localPlugins/email-cta-insert/index.js
@@ -1,25 +1,17 @@
 const visit = require('unist-util-visit');
 
 module.exports = (options) => {
-
     var acHtml = `
     <div class="border-solid border-4 container-inner mx-auto py-16 pb-8 text-center textl-xl">
     <h2 class="text-2xl font-bold mb-8">Live stream notifications? New posts? Thought-provoking conversations? Sign up today!</h2>
 
     <div class="w-4/5 mx-auto mb-8">
-      <form method="POST" action="https://swiftkick.activehosted.com/proc.php" id="_form_10_">
-        <input type="hidden" name="u" value="10" />
-        <input type="hidden" name="f" value="10" />
-        <input type="hidden" name="s" />
-        <input type="hidden" name="c" value="0" />
-        <input type="hidden" name="m" value="0" />
-        <input type="hidden" name="act" value="sub" />
-        <input type="hidden" name="v" value="2" />
-        <input type="hidden" id="currentPageField" name="field[8]" />
+      <form id="ctaForm">
         <div>
           <input
             type="text"
             name="fullname"
+            id="ctaFormFullName"
             placeholder="Your name"
             class="w-full bg-background-form rounded sm:rounded-r-none px-4 py-4 leading-normal border border-border-color-primary sm:border-r-0 shadow outline-none focus:border-green-700 z-10"
             required
@@ -28,12 +20,14 @@ module.exports = (options) => {
           <input
             type="email"
             name="email"
+            id="ctaFormEmailAddress"
             placeholder="Your email address"
             class="w-full bg-background-form rounded sm:rounded-r-none px-4 py-4 leading-normal border border-border-color-primary sm:border-r-0 shadow outline-none focus:border-green-700 z-10"
             required
           />
           </div>
           <button
+            id="ctaFormButton"
             data-element="submit"
             class="w-40 uppercase bg-green-700 text-white rounded text-lg py-3 px-8 tracking-wide shadow focus:outline-none hover:bg-green-800 focus:bg-green-800 z-10 w-full sm:w-auto mt-5"
           >
