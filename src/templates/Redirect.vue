2c8afed4de8f315c41bbc9ca14429e55f688812c
Kevin Griffin <kevin@kevgriffin.com>
2020-09-25 10:00:15 -0400

    updatE (#44)

---
+++ src/templates/Redirect.vue
@@ -0,0 +1,22 @@
+<template>
+  <p>Redirecting to home page...</p>
+</template>
+
+<script>
+export default {
+  metaInfo() {
+    return {
+      title: `Redirecting to home page...`,
+      tag: {
+          title: "Redirect"
+      },
+      meta: [
+        {
+          "http-equiv": "refresh",
+          content: `0; URL=/`
+        }
+      ]
+    };
+  }
+};
+</script>
