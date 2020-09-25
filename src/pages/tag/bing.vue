a0876186cb3e5c61243219b5cc7369f836e54cbe
Kevin Griffin <kevin@kevgriffin.com>
2020-09-24 23:45:08 -0400

    404 fixes

---
+++ src/pages/tag/bing.vue
@@ -0,0 +1,19 @@
+<template>
+  <p>Redirecting to home page</p>
+</template>
+
+<script>
+export default {
+  metaInfo() {
+    return {
+      title: `Redirecting to updated article...`,
+      meta: [
+        {
+          "http-equiv": "refresh",
+          content: `0; URL=/`
+        }
+      ]
+    };
+  }
+};
+</script>
