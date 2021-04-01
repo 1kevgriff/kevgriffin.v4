f4476a2e180e7b55192be9ddd9871937aab17c6c
Kevin Griffin <kevin@kevgriffin.com>
2021-04-01 15:33:47 -0400

    wip

---
+++ src/pages/Hire.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <!-- Calendly inline widget begin -->
+    <div
+      class="calendly-inline-widget"
+      data-url="https://calendly.com/1kevgriff/introduction"
+      style="min-width: 320px; height: 630px"
+    ></div>
+    <script
+      type="text/javascript"
+      src="https://assets.calendly.com/assets/external/widget.js"
+    ></script>
+    <!-- Calendly inline widget end -->
+  </div>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: "Let's Work Together",
+    description: "Kevin W. Griffin | Developer, Training, Entrepreneur",
+  },
+};
+</script>
+
+<style>
+</style>
