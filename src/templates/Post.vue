8af7c13fe9b528a31a1248bdb88f13b706640eeb
Kevin Griffin <kevin@solliance.net>
2021-12-07 09:47:40 -0500

    init previewify

--- src/templates/Post.vue
+++ src/templates/Post.vue
@@ -31,6 +31,7 @@ query Post ($path: String!) {
     summary
     content
     excerpt
+    timeToRead
     tags {
       title
       path
@@ -52,9 +53,20 @@ export default {
         { name: "twitter:site", content: "@1kevgriff" },
         {
           name: "twitter:image",
-          content: "https://consultwithgriff.com" + kevin_rocks,
+          content:
+            "https://previewify.app/i/769?url=https://consultwithgriff.com" +
+            this.$page.post.path,
         },
         { name: "twitter:creator", content: "@1kevgriff" },
+        {
+          name: "og:image",
+          content:
+            "https://previewify.app/i/769?url=https://consultwithgriff.com" +
+            this.$page.post.path,
+        },
+        { name: "previewify:title", content: this.$page.post.title},
+        { name: "previewify:reading_time", content: timeToRead},
+        { name: "previewify:category", content: ""}
         {
           name: "description",
           content:
