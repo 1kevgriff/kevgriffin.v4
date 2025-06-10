50cd00b1c19a25ea63d1cc6718a6ce3c68fc4f79
Kevin Griffin <kevin@kevgriffin.com>
2025-06-10 09:28:04 -0400

    tweka

--- src/templates/Post.vue
+++ src/templates/Post.vue
@@ -3,15 +3,6 @@
     <div class="mx-auto my-16 container-inner">
       <h2 class="text-4xl font-bold leading-tight">{{ $page.post.title }}</h2>
       <div class="mb-4 text-xl text-gray-600">{{ $page.post.date }}</div>
-      <div class="flex mb-8 text-sm">
-        <g-link
-          :to="tag.path"
-          v-for="tag in $page.post.tags"
-          :key="tag.id"
-          class="px-4 py-2 mr-4 bg-gray-300 rounded-full hover:bg-green-300"
-          >{{ tag.title }}</g-link
-        >
-      </div>
       <div class="mb-8 markdown-body" v-html="$page.post.content" />
       <div class="mb-8">
         <g-link to="/articles" class="font-bold uppercase"
